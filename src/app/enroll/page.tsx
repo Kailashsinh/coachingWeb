5972c88488cfe9d7b75897ea71a4248e67767592
Kailashsinh <kailashsinhrajput25@gmail.com>
2025-07-20 10:04:34 +0530

    Add files via upload

---
+++ src/app/enroll/page.tsx
@@ -0,0 +1,9 @@
+import EnrollForm from "@/components/enroll";
+
+export default function EnrollPage() {
+  return (
+    <main className="min-h-screen p-4 bg-gray-50">
+      <EnrollForm />
+    </main>
+  );
+}
