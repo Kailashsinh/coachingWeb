68c4691e8cd94d1d36abba8028430a24be0642d7
Kailashsinh <kailashsinhrajput25@gmail.com>
2025-07-20 10:03:21 +0530

    Delete enroll directory

--- enroll/page.tsx
+++
@@ -1,9 +0,0 @@
-import EnrollForm from "@/components/enroll";
-
-export default function EnrollPage() {
-  return (
-    <main className="min-h-screen p-4 bg-gray-50">
-      <EnrollForm />
-    </main>
-  );
-}
